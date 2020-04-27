b64c2035904b4f6bd0b112e07748ee5be788d27c
Jeffrey Valdez <53051337+jvaldezdipre@users.noreply.github.com>
2020-04-27 15:44:17 -0400

    Created the Footer and styled with material ui

--- use_my_tech/src/App.js
+++ use_my_tech/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './App.css';
-
 import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
+import './App.css';
 
 function App() {
   return (
     <Router>
       <div className='App'>
         <Navbar />
+        <Footer />
         <Switch></Switch>
       </div>
     </Router>
