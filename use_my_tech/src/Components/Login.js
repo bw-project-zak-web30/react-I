c92c500ec8e20e32427131d4d279a4fe47bef302
Hunter Stevens <hunterstevens1996@gmail.com>
2020-04-28 11:57:59 -0600

    added functionality to Signup and Login

--- use_my_tech/src/Components/Login.js
+++ use_my_tech/src/Components/Login.js
@@ -1,6 +1,44 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import AxiosWithAuth from '../Utils/AxiosWithAuth';
+
+import '../styles/login.css';
+
+// ----------MATERIAL UI IMPORTS------------------
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+// import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.primary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 const Login = () => {
   const [loginState, setLoginState] = useState({
@@ -17,45 +55,66 @@ const Login = () => {
 
   const loginSubmit = ev => {
     ev.preventDefault();
+    console.log("LoginSubmit activated")
 
-    axios
-      .post('', loginState)
+    AxiosWithAuth()
+      .post('/api/auth/login', loginState)
       .then(res => {
         console.log('Login Fetch: ', res);
       })
       .catch(err => console.log('Login ERROR: ', err));
   };
 
-  return (
-    <div>
-    <form onSubmit={loginSubmit}>
-      <label htmlFor='username'>
-        <input
-          label='username'
-          type='text'
-          name='username'
-          value={loginState.username}
-          onChange={handleChange}
-          placeholder='username'
-        />
-      </label>
+  //----Material ui styles----------
+  const classes = useStyles();
 
-      <label htmlFor='password'>
-        <input
-          label='password'
-          type='password'
-          name='password'
-          value={loginState.password}
-          onChange={handleChange}
-          placeholder='Password'
-        />
-      </label>
-      <button>Login</button>
-    </form>
+  return (
+    <Container component='main' maxWidth='xs'>
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component='h1' variant='h5'>
+          Sign in
+        </Typography>
+        <form onSubmit={loginSubmit}>
+          {/* <label htmlFor='username'>
+          </label> */}
+          <TextField
+            label='username'
+            type='text'
+            name='username'
+            value={loginState.username}
+            onChange={handleChange}
+            placeholder='username'
+            fullWidth
+            variant='outlined'
+            margin='normal'
+          />
+
+          {/* <label htmlFor='password'>
+          </label> */}
+          <TextField
+            label='password'
+            type='password'
+            name='password'
+            value={loginState.password}
+            onChange={handleChange}
+            placeholder='Password'
+            fullWidth
+            variant='outlined'
+            margin='normal'
+          />
+          <Button type="submit" fullWidth variant='contained' className={classes.submit}>
+            Login
+          </Button>
+        </form>
     <div>
       <p>Don't have an account? </p><Link to="/register">Sign-Up Here!</Link>
     </div>
-</div>
+      </div>
+    </Container>
   );
 };
 
--- use_my_tech/src/Components/Signup.js
+++ use_my_tech/src/Components/Signup.js
@@ -1,87 +1,149 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import AxiosWithAuth from '../Utils/AxiosWithAuth';
 
+//-------MATERIAL UI IMPORTS---------
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+// import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
-const Signup = () =>{
-    const [signState, setSignState] = useState({
-        userType:'',
-        name:'',
-        userName:'',
-        password:'',
-        city:'',
-        id:''
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.primary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+const Signup = () => {
+  const [signState, setSignState] = useState({
+    username: '',
+    password: '',
+    name: '',
+    city: ''
+  });
+
+  const handleChange = ev => {
+    ev.preventDefault();
+
+    setSignState({
+      ...signState,
+      [ev.target.name]: ev.target.value,
+    });
+  };
+
+  const signUpSubmit = ev => {
+    ev.preventDefault();
+
+    console.log(signState)
+
+    AxiosWithAuth().post('/api/auth/register', signState)
+    .then(res =>{
+      console.log("New User API: ", res);
+
     })
+    .catch(err =>{
+      console.log("Sign up submit ERROR: ", err.message.response);
+    })
+  };
 
-    
-    const handleChange = ev =>{
-        ev.preventDefault();
+  //----------MATERIAL UI STYLES ------
+  const classes = useStyles();
 
-        setSignState({
-            ...signState,
-            [ev.target.name]:ev.target.value
-        })
-    }
-
-
-    const signUpSubmit = ev =>{
-        ev.preventDefault();
-    }
-
-    return(
-      <div>
+  return (
+    <Container component='main' maxWidth='xs'>
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component='h1' variant='h5'>
+          Sign up
+        </Typography>
         <form onSubmit={signUpSubmit}>
-            <select id="userType" name ="userType"
+          <TextField
+            label='Name'
+            id='name'
+            name='name'
+            placeholder='Full name with spaces'
             onChange={handleChange}
-            value={signState.userType}>
-                <option value="">--Customer or Equipment Owner--</option>
-                <option value="customer">Customer</option>
-                <option value="equipment owner">Equipment Owner</option>
-            </select>
-            <input
-          label="Name"
-          id="name"
-          name="name"
-          placeholder="Full name with spaces"
-          onChange={handleChange}
-          value={signState.name}
-          required
-        />
-        <input
-          label="City"
-          id="city"
-          name="city"
-          placeholder="Users City"
-          onChange={handleChange}
-          value={signState.city}
-          required
-        />
-        <input
-          label="Username"
-          id="username"
-          name="username"
-          placeholder="username"
-          onChange={handleChange}
-          value={signState.userName}
-          required
-        />
-        <input
-          label="Password"
-          type="password"
-          id="password"
-          name="password"
-          placeholder="password"
-          onChange={handleChange}
-          value={signState.password}
-          required
-        />
-        <button>Sign Up</button>
+            value={signState.name}
+            required
+            fullWidth
+            variant='outlined'
+            margin='normal'
+          />
+          <TextField
+            label='City'
+            id='city'
+            name='city'
+            placeholder='Users City'
+            onChange={handleChange}
+            value={signState.city}
+            required
+            fullWidth
+            variant='outlined'
+            margin='normal'
+          />
+          <TextField
+            label='Username'
+            id='username'
+            name='username'
+            placeholder='username'
+            onChange={handleChange}
+            value={signState.username}
+            required
+            fullWidth
+            variant='outlined'
+            margin='normal'
+          />
+          <TextField
+            label='Password'
+            type='password'
+            id='password'
+            name='password'
+            placeholder='password'
+            onChange={handleChange}
+            value={signState.password}
+            required
+            fullWidth
+            variant='outlined'
+            margin='normal'
+          />
+          <Button type="submit" fullWidth variant='contained' className={classes.submit}>
+            Sign Up
+          </Button>
         </form>
         <div>
-          <p>Already have an account? </p><Link to="/login">Go ahead and Login here!</Link>
+          <p>Already have an account? </p>
+          <Link to='/login'>Go ahead and Login here!</Link>
         </div>
       </div>
-    )
-}
+    </Container>
+  );
+};
 
 export default Signup;
