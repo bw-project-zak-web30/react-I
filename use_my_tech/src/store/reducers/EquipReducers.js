820e830a19dc16bdfa6a4dcff55dd1b35f39e989
Hunter Stevens <hunterstevens1996@gmail.com>
2020-04-29 15:05:18 -0600

    Made the Reducers

---
+++ use_my_tech/src/store/reducers/EquipReducers.js
@@ -0,0 +1,31 @@
+const initialState = {
+    equipments:[],
+    isFetching:false,
+    error:'',
+}
+
+export const EquipReducer = (state = initialState, action) =>{
+    switch(action.type){
+        case 'FETCH_ITEMS_START':
+            return{
+                ...state,
+                isFetching:true
+            }
+
+        case 'FETCH_ITEMS_SUCCESS':
+            return{
+                ...state,
+                equipments:action.payload,
+                isFetching:false,
+                error:''
+            }
+        
+
+        case 'FETCH_ITEMS_FAIL':
+            return{
+                ...state,
+                isFetching:false,
+                error:action.payload
+            }
+    }
+}
