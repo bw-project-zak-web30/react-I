9459dadbb4c5ce8496033739ccb8252c382b61ab
Hunter Stevens <hunterstevens1996@gmail.com>
2020-04-29 11:09:47 -0600

    rentalPage renting function working and add timeframe to RentalCard

--- use_my_tech/src/Components/RentalCard.js
+++ use_my_tech/src/Components/RentalCard.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-function RentalCard({ product }) {
+function RentalCard({ product, rentNow }) {
   const isRented = () => {
     if (product.renting === false) {
-      return <button>Rent this Now</button>;
+      return <button onClick={() =>{rentNow(product)}}>Rent this Now</button>;
     } else {
       return <p>Some nice person is currently renting this product</p>;
     }
@@ -14,6 +14,7 @@ function RentalCard({ product }) {
       <h2>{product.name}</h2>
       <p>Price: {product.price}</p>
       <p>Details: {product.details}</p>
+      <p>Rent Timeframe: {product.timeframe} days</p>
       {/* If statement for if product is being rented or not */}
       {isRented()}
     </div>
--- use_my_tech/src/Components/RentalPage.js
+++ use_my_tech/src/Components/RentalPage.js
@@ -3,12 +3,41 @@ import RentalCard from './RentalCard';
 import AxiosWithAuth from '../Utils/AxiosWithAuth';
 
 function RentalPage() {
+  const userId = localStorage.getItem('userId');
 
+  const today = new Date().toISOString();
+
+  const returnRent = theDate =>{
+    console.log(theDate);
+
+    let newDay = new Date();
+    newDay.setDate(newDay.getDate() + theDate);
+    let dateString = newDay.toISOString();
+    let returnDay = dateString.slice(0,10);
+    return returnDay;
+    
+  }
   //--------------------State--------------------
   const [rentals, setRentals] = useState([]);
 
+  const [itemRent, setItemRent] = useState(
+    {
+      return_date: '',
+      start_date: today.slice(0,10),
+      details: rentals.details,
+      renter_id: parseInt(userId),
+      owner_id: rentals.owner_id,
+      equipment_id: rentals.id
+    }
+  )
+
+    console.log(itemRent);
   //-------------------Backend Call--------------
   useEffect(() => {
+    getRentals();
+  }, []);
+  
+  const getRentals = () =>{
     AxiosWithAuth()
       .get('/api/equipment')
       .then(res => {
@@ -18,14 +47,36 @@ function RentalPage() {
       .catch(err => {
         console.log('Could not grab the equipments', err);
       });
-  }, []);
+  }
+
+
+  const rentNow = (item) => {
+    setItemRent({
+      ...itemRent,
+      return_date:returnRent(item.timeframe),
+      details:item.details,
+      owner_id:item.owner_id,
+      equipment_id:item.id
+    });
+
+    console.log(itemRent);
+
+    AxiosWithAuth().post(`/api/equipment`, itemRent)
+    .then(res =>{
+      console.log(res);
+      getRentals();
+    })
+    .catch(err =>{
+      console.log(err);
+    })
+  }
 
 
   return (
     <div>
       <h1>I am the Rental page here will show all the equipments for rental</h1>
       {rentals.map(rental => {
-        return <RentalCard key={rental.id} product={rental} />;
+        return <RentalCard key={rental.id} product={rental} rentNow={rentNow} />;
       })}
     </div>
   );
